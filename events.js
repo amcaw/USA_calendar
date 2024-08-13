23dc78283e7fea7c9c7590ed8c21347138a41dcb
amcaw <42608053+amcaw@users.noreply.github.com>
2024-08-13 13:15:06 +0200

    Create events.js

---
+++ events.js
@@ -0,0 +1,12 @@
+// events.js
+export const events = [
+    { date: '19/8/2024', type: 'dem', title: "Démocrates", description: "Convention démocrate à Chicago pour désigner le candidat à l'élection présidentielle" },
+    { date: '20/8/2024', type: 'dem', title: "Démocrates", description: "Convention démocrate à Chicago pour désigner le candidat à l'élection présidentielle" },
+    { date: '21/8/2024', type: 'dem', title: "Démocrates", description: "Convention démocrate à Chicago pour désigner le candidat à l'élection présidentielle" },
+    { date: '22/8/2024', type: 'dem', title: "Démocrates", description: "Convention démocrate à Chicago pour désigner le candidat à l'élection présidentielle" },
+    { date: '6/9/2024', type: 'state', title: "Fédéral", description: 'Début du vote par courrier dans certains États' },
+    { date: '10/9/2024', type: 'multi', title: "Débat présidentiel", description: 'Débat présidentiel entre les candidats démocrate et républicain sur la chaîne ABC' },
+    { date: '20/9/2024', type: 'state', title: "Fédéral", description: 'Début du vote anticipté dans certains États' },
+    { date: '5/11/2024', type: 'election', title: "Fédéral", description: 'Élection présidentielle' }
+  ];
+  
